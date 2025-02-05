a48547dfa07ce441d4c5b75916c2cdb222409576
Huynh Cac Duy Thuan <20130423@st.hcmuaf.edu.vn>
2025-02-05 23:11:34 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     }
 
     try {
-      const response = await fetch('https://flask-backend-2-k9zi.onrender.com/shorten', {
+      const response = await fetch('https://flask-backend-3-ff3g.onrender.com/shorten', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -50,7 +50,7 @@ const App = () => {
     }
   
     try {
-      const response = await fetch(`https://flask-backend-2-k9zi.onrender.com/update/${shortenedUrl.split('/')[1]}`, {
+      const response = await fetch(`https://flask-backend-3-ff3g.onrender.com/update/${shortenedUrl.split('/')[1]}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -76,7 +76,7 @@ const App = () => {
 
   // Copy to clipboard function
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(`https://flask-backend-2-k9zi.onrender.com${shortenedUrl}`);
+    navigator.clipboard.writeText(`https://flask-backend-3-ff3g.onrender.com${shortenedUrl}`);
    
   };
 
